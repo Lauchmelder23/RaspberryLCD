63f674dcafe9bea8cfbece3d5edc7414303d5668
Lauchmelder23 <robert.trololo@gmail.com>
2020-11-20 13:59:27 +0000

    Updated example

--- example/example.c
+++ example/example.c
@@ -6,13 +6,7 @@ int main(void)
     configurePins(&screen, 7, 9, 8, 0, 0, 0, 0, 21, 22, 23, 24);
 
     initScreen(&screen, HALF_BYTE_INTERFACE, TWO_LINES, FONT_5x7, CURSOR_ON | CURSOR_BLINK, LEFT_TO_RIGHT);
-    sendText(&screen, "スウェーデン");
-    setCursor(&screen, 0, 1);
-    sendText(&screen, "オーストラリア");
-    shiftScreen(&screen, RIGHT_SHIFT);
-    shiftScreen(&screen, RIGHT_SHIFT);
-    shiftScreen(&screen, RIGHT_SHIFT);
-    setDisplaySettings(&screen, DISPLAY_ON, CURSOR_OFF, CURSOR_STATIC);
+    sendText(&screen, "ゴチソウサマデシタ");
     resetPins(&screen);
 
     return 0;
