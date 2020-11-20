19dc5c00b4e127176f976ce65ef76a80680d14d2
Lauchmelder23 <robert.trololo@gmail.com>
2020-11-20 11:50:52 +0000

    Added utf8 encoding for katakan

--- example/example.c
+++ example/example.c
@@ -6,7 +6,9 @@ int main(void)
     configurePins(&screen, 7, 9, 8, 0, 0, 0, 0, 21, 22, 23, 24);
 
     initScreen(&screen, HALF_BYTE_INTERFACE, TWO_LINES, FONT_5x7, CURSOR_ON | CURSOR_BLINK, LEFT_TO_RIGHT);
-    sendText(&screen, "カガ　キギクグケゲコゴ");
+    sendText(&screen, "スウェーデン");
+    sendCommand(&screen, DDRAM_AD_SET | 0x40);
+    sendText(&screen, "オーストラリア");
     resetPins(&screen);
 
     return 0;
